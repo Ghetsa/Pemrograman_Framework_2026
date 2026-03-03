7e9133f2747d0f0c2e81f9d93397520f1d418022
Ghetsa Ramadhani <ghetsa.arryanti@gmail.com>
2026-03-03 21:21:07 +0700

    add: langkah 8 (Konfigurasi Firebase)

--- jobsheet-07/my-app/src/utils/db/firebase.ts
+++ jobsheet-07/my-app/src/utils/db/firebase.ts
@@ -4,14 +4,13 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDuO0PN-yGjEU1iQBOoLPcf9PsRNUchPfE",
-  authDomain: "framework-next-ghetsa.firebaseapp.com",
-  projectId: "framework-next-ghetsa",
-  storageBucket: "framework-next-ghetsa.firebasestorage.app",
-  messagingSenderId: "112842951336",
-  appId: "1:112842951336:web:fcbe26a6b01c322fbe509d"
-};
+const firebaseConfig = { 
+  apiKey: process.env.FIREBASE_API_KEY, 
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN, 
+  projectId: process.env.FIREBASE_PROJECT_ID, 
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET, 
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID, 
+  appId: process.env.FIREBASE_APP_ID };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);

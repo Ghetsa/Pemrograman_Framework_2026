ffd8caa379ed7ddd5b5ddce45c8703df43b59298
Ghetsa Ramadhani <ghetsa.arryanti@gmail.com>
2026-03-06 22:16:47 +0700

    add: jobsheet 10 (Static Site Generation (SSG))

---
+++ jobsheet-10/my-app/src/pages/produk/[id].tsx
@@ -0,0 +1,5 @@
+import ProdukDetailView from "../views/product/produk-detail";
+
+export default function ProdukDetailPage() {
+  return <ProdukDetailView />;
+}
---
+++ jobsheet-10/my-app/src/pages/profile/edit/index.tsx
@@ -0,0 +1,7 @@
+import ProfileEditView from "@/views/profile/edit";
+
+const ProfileEditPage = () => {
+  return <ProfileEditView />;
+};
+
+export default ProfileEditPage;

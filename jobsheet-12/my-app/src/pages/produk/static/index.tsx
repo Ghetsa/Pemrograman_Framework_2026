bf850c68ad992b83cfa7dc170fe6d1a355fcf80c
Ghetsa Ramadhani <ghetsa.arryanti@gmail.com>
2026-03-07 23:33:13 +0700

    add: Bagian 1 (Tambahkan revalidate)

--- jobsheet-12/my-app/src/pages/produk/static/index.tsx
+++ jobsheet-12/my-app/src/pages/produk/static/index.tsx
@@ -16,11 +16,13 @@ export default HalamanProdukStatic;
 
 export async function getStaticProps() {
   const res = await fetch("http://127.0.0.1:3000/api/produk");
-  const response = await res.json();
+  // const response = await res.json();
+  const response: {data: ProductType[]} = await res.json();
 
   return {
     props: {
       products: response.data,
     },
+    revalidate: 10,
   };
 }

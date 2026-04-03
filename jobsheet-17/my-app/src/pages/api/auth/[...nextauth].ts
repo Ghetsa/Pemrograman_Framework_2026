45307acf42471558cab77e49938ce92c8e455dbe
Ghetsa Ramadhani <ghetsa.arryanti@gmail.com>
2026-04-03 13:07:29 +0700

    add: Bagian 3 - Konfigurasi Google Provider di NextAuth dan Handle Callback JWT & Session

--- jobsheet-17/my-app/src/pages/api/auth/[...nextauth].ts
+++ jobsheet-17/my-app/src/pages/api/auth/[...nextauth].ts
@@ -4,6 +4,7 @@ import { signIn } from "@/utils/db/servicefirebase";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
   session: {
@@ -41,6 +42,10 @@ export const authOptions: NextAuthOptions = {
         return null;
       },
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
   ],
 
   callbacks: {
@@ -51,6 +56,21 @@ export const authOptions: NextAuthOptions = {
         token.role = user.role;
       }
       //  console.log("jwt callback", { token, account, profile, user })
+      
+      // Jika login dengan Google, tambahkan informasi yang diperlukan ke token
+      if (account?.provider === "google") {
+        const data = {
+          fullname: user.name,
+          email: user.email,
+          image: user.image,
+          type: account.provider,
+        };
+        // console.log("Google login data", { data });
+        token.fullname = data.fullname;
+        token.email = data.email;
+        token.image = data.image;
+        token.type = data.type;
+      }
       return token;
     },
     async session({ session, token }: any) {
@@ -60,8 +80,14 @@ export const authOptions: NextAuthOptions = {
       if (token.fullname) {
         session.user.fullname = token.fullname;
       }
+      if (token.image) {
+        session.user.image = token.image;
+      }
       if (token.role) {
         session.user.role = token.role;
+      }
+      if (token.type) {
+        session.user.type = token.type;
       }
       // console.log("session callback", { session, token })
       return session;
